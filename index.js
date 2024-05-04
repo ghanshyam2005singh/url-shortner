2ec84708764c8010711a82b3a8dc4f7295a9c0ed
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2024-05-05 00:37:43 +0530

    initializing

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ const express = require("express");
 const path = require("path");
 const {connectToMongoDB} = require("./connect");
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 
 const app = express();
 const port = 8001;
 
-connectToMongoDB("mongodb://localhost:127.0.0.1:27017/short-url").then(
+connectToMongoDB("mongodb://localhost:127.0.0.1:27017/short-url").then(() =>
     console.log("MongoDB connected")
 );
-
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(port, () => console.log(`Server is running at PORT:${port}`));
